0309936f829f2a529975cf79ea944f28bcc4fbf6
grvx_git <gaurav.4source@gmail.com>
2023-07-05 15:58:51 +0530

    styled nav

--- src/MyComponents/header.jsx
+++ src/MyComponents/header.jsx
@@ -1,30 +1,33 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import { useState } from "react";
+import Logo from "../assets/logo.png"
+// const [activeLink, setActiveLink] = useState('home')
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Our Services</Nav.Link>
+            <Nav.Link href="#link">Team</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="socials">
+              <a href="#">Call Us: +1 (514) 022-8419</a>
+              <a href="#">Log In</a>
+							<button className="signUp" >Sign Up</button>
+            </div>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
